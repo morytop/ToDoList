6a8a3cbd344432ee1f31487ee7abbd3237c680fc
Piotr Moryto <piotrmoryto@gmail.com>
2021-04-17 14:19:20 +0200

    add approve, edit and delete buttons

--- main.js
+++ main.js
@@ -26,12 +26,36 @@ const addNewTask = () => {
     if ($todoInput.value !== '') {
         $newTask = document.createElement('li');
         $newTask.innerText = $todoInput.value;
+
         $ulList.appendChild($newTask);
         $todoInput.value = '';
         $alertInfo.innerText = '';
+        createToolsArea();
     } else {
         $alertInfo.innerText = 'Wpisz treść zadania!';
     }
 };
+
+const createToolsArea = () => {
+    const toolsPanel = document.createElement('div');
+    toolsPanel.classList.add('tools');
+    $newTask.appendChild(toolsPanel);
+
+    const completeBtn = document.createElement('button');
+    completeBtn.classList.add('complete');
+    completeBtn.innerHTML = '<i class="fas fa-check"></i>';
+
+    const editBtn = document.createElement('button');
+    editBtn.classList.add('edit');
+    editBtn.innerText = 'EDIT';
+
+    const deleteBtn = document.createElement('button');
+    deleteBtn.classList.add('delete');
+    deleteBtn.innerHTML = '<i class="fas fa-times"></i>';
+
+    toolsPanel.appendChild(completeBtn);
+    toolsPanel.appendChild(editBtn);
+    toolsPanel.appendChild(deleteBtn);
+}
 
 document.addEventListener('DOMContentLoaded', main);
